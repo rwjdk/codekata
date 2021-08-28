b18808acc0ac2d3a9ec6cb9667105f9069515f1a
Rasmus Wulff Jensen <rasmus.wulff.jensen@outlook.com>
2021-08-28 19:57:39 +0200

    TestOdata

---
+++ WebApplicationOData/WebApplicationOData/Controllers/EmployeeController.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
+using WebApplicationOData.Models;
+using WebApplicationOData.Services;
+
+namespace WebApplicationOData.Controllers
+{
+    [Route("emp")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly EmployeeService _employeeService;
+
+        public EmployeeController(EmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        [HttpGet]
+        [EnableQuery]
+        public IActionResult Get()
+        {
+            return Ok(_employeeService.GetEmployees());
+        }
+
+        [HttpGet("GetByKey")]
+        [EnableQuery]
+        public IActionResult Get(string key)
+        {
+            var firstOrDefault = _employeeService.GetEmployees().FirstOrDefault(x => x.Id.ToString() == key);
+            return Ok(firstOrDefault);
+        }
+        /*
+        [HttpPost]
+        [EnableQuery]
+        public IActionResult Post([FromBody] EmployeeModel postObject)
+        {
+            return Ok();
+        }*/
+    }
+}
---
+++ WebApplicationOData/WebApplicationOData/Models/EmployeeModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplicationOData.Models
+{
+    public class EmployeeModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Role { get; set; }
+        public string City { get; set; }
+        public int Pincode { get; set; }
+    }
+}
