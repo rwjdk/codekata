4123a96ec6c2cd83de4ba5ec326f77aef102d6ca
Rasmus Wulff Jensen <rwj@boyum-it.com>
2021-08-30 12:39:28 +0200

    Added SharedController Sample

---
+++ BlazorGenericsAndReflection/BlazorGenericsAndReflection/BusinessLogic/Interface/IFeatureShared.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorGenericsAndReflection.BusinessLogic.Interface
+{
+    public interface IFeatureShared<out T> where T: IFeatureOutput
+    {
+        string Name { get; }
+        T Output { get; }
+        void ActionOnButtonClick();
+
+    }
+}
---
+++ BlazorGenericsAndReflection/BlazorGenericsAndReflection/BusinessLogic/Services/SharedImplementation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using BlazorGenericsAndReflection.BusinessLogic.Interface;
+
+namespace BlazorGenericsAndReflection.BusinessLogic.Services
+{
+    public class SharedImplementation : IFeatureShared<IFeatureOutput>
+    {
+        private readonly Action _actionOnButtonClick;
+        public string Name { get; }
+        public IFeatureOutput Output { get; }
+        public void ActionOnButtonClick()
+        {
+            _actionOnButtonClick.Invoke();
+        }
+
+        public SharedImplementation(string name, IFeatureOutput output, Action actionOnButtonClick)
+        {
+            _actionOnButtonClick = actionOnButtonClick;
+            Name = name;
+            Output = output;
+        }
+
+        public SharedImplementation()
+        {
+            //Empty
+        }
+    }
+}
